2a36f35f87b4b5850b16ebc2d8525574d7b49157
Piotrowski <maciekp102@gmail.com>
2020-12-03 15:17:04 +0100

    start

---
+++ src/server.java
@@ -0,0 +1,8 @@
+import java.net.*;
+import java.io.*;
+
+public class server {
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("localhost", 4999);
+    }
+}
